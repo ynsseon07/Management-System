23672764041321dab94591020080d53640a98bea
ynsseon <ynsseon@gmail.com>
2021-02-23 22:08:22 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,46 +1,24 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component {
     // Customer 컴포넌트가 화면에 그리고자 하는 내용을 render() 안에 작성
     render() {
         return(
-            <div>
-                <CustomerProfile 
-                    id={this.props.id}
-                    image={this.props.image}
-                    name={this.props.name}
-                />
-                <CustomerInfo
-                    birth={this.props.birth}
-                    gender={this.props.gender}
-                    job={this.props.job}
-                />
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.img} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birth}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+            
+               
         )
         
     }
 }
 
-class CustomerProfile extends React.Component {
-    render() {
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render() {
-        return(
-            <div>
-                <p>{this.props.birth}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        )
-    }
-}
 export default Customer;
