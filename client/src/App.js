976cbf03f3a9633ff27a891dc08467dc3c6209e5
ynsseon <ynsseon@gmail.com>
2021-02-23 23:13:27 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '성춘향',
-  'birthday': '980520',
-  'gender': '여자',
-  'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '홍길동',
-    'birthday': '920105',
-    'gender': '남자',
-    'job': '대학원생'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '이몽룡',
-    'birthday': '941210',
-    'gender': '남자',
-    'job': '직장인'
-  }
-]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -66,22 +56,22 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {
-            // 반복문 처리하기 위해 map 사용
-            customers.map(c => {
-              return (
-                <Customer
-                  key={c.id}  // 구분할 수 있는 고유값
-                  id={c.id}
-                  img={c.image}
-                  name={c.name}
-                  birth={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })
-            }
+            
+              {/* 반복문 처리하기 위해 map 사용 */}
+              {this.state.customers ? this.state.customers.map(c => {
+                return (
+                  <Customer
+                    key={c.id}  // 구분할 수 있는 고유값
+                    id={c.id}
+                    img={c.image}
+                    name={c.name}
+                    birth={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              }) : "" }
+            
           </TableBody>
         </Table>
         
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '성춘향',
+            'birthday': '980520',
+            'gender': '여자',
+            'job': '대학생'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '홍길동',
+            'birthday': '920105',
+            'gender': '남자',
+            'job': '대학원생'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '이몽룡',
+            'birthday': '941210',
+            'gender': '남자',
+            'job': '직장인'
+        }
+    ]);
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
